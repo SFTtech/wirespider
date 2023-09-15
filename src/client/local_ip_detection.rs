b56134f54acc2bd67c827daa63172259c7924dcf
Linus Karl <linus.karl@eso.org>
2023-09-15 13:35:30 +0200

    bump version of boringtun

--- src/client/local_ip_detection.rs
+++ src/client/local_ip_detection.rs
@@ -46,7 +46,8 @@ async fn check_ip(
     let socket = UdpSocket::bind("0.0.0.0:0").await?;
     socket.connect(dest).await?;
     let mut buffer = [0u8; 148]; // size from boringtun::noise::HANDSHAKE_INIT_SZ
-    let mut tun = Tunn::new(priv_key, pub_key, None, None, 1, None).unwrap_or_log();
+    let tun = Tunn::new(priv_key, pub_key, None, None, 1, None).unwrap_or_log();
+    tokio::pin!(tun);
     match tun.format_handshake_initiation(&mut buffer, false) {
         boringtun::noise::TunnResult::Err(_) => return Ok(None),
         boringtun::noise::TunnResult::WriteToNetwork(buf) => {
